ac8d1ed4351cd759a1cc5f6ad0ddf3543abf836a
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-06-29 11:33:14 +0100

    Responsiveness

--- components/faq/faq-item/index.tsx
+++ components/faq/faq-item/index.tsx
@@ -1,28 +1,28 @@
-"use client";
-import React, { useState } from "react";
-import Image from "next/image"; 
-import plusIcon from "@/public/FAQ/plus.svg";
-import minusIcon from "@/public/FAQ/minus.svg"; 
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import plusIcon from '@/public/FAQ/plus.svg'
+import minusIcon from '@/public/FAQ/minus.svg'
 
 interface FAQItemProps {
-    faq: {
-        id: number;
-        question: string;
-        answer: string;
-    };
+  faq: {
+    id: number
+    question: string
+    answer: string
+  }
 }
 
 const FAQItem = ({ faq }: FAQItemProps) => {
-  const [active, setActive] = useState<boolean>(false);
+  const [active, setActive] = useState<boolean>(false)
 
   const toggleAccordion = () => {
-    setActive(!active);
-  };
+    setActive(!active)
+  }
 
   return (
     <div className="border-b border-[#D6D6D6] ease-out duration-300  lg:max-w-[795px] lg:pb-4 ">
       <div className="flex justify-between items-center lg:gap-x-4">
-        <h5 className=" font-normal  lg:text-[24px] 2xl:text-[32px]">
+        <h5 className="  font-medium lg:font-normal lg:text-[24px] 2xl:text-[32px]">
           {faq.question}
         </h5>
         {!active ? (
@@ -42,10 +42,12 @@ const FAQItem = ({ faq }: FAQItemProps) => {
         )}
       </div>
       {active && (
-        <p className="lg:mt-4 lg:leading-8 lg:text-base">{faq.answer}</p>
+        <p className="text-sm mt-2 leading-6 lg:mt-4 lg:leading-8 lg:text-base">
+          {faq.answer}
+        </p>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default FAQItem;
+export default FAQItem
