a941045d855a81fb0984a6ff483d3b8e2e7d39cd
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-06-29 11:32:39 +0100

    Responsiveness

--- components/cta/index.tsx
+++ components/cta/index.tsx
@@ -1,29 +1,34 @@
-import Image from "next/image";
-import illustRight from "@/public/CTA/right-illust.svg";
-import illustLeft from "@/public/CTA/left-illust.svg";
+import Image from 'next/image'
+import illustRight from '@/public/CTA/right-illust.svg'
+import illustLeft from '@/public/CTA/left-illust.svg'
 
 const CTA = () => {
-  
   return (
-    <section className="relative shorten w-full text-white flex flex-col justify-center items-center lg:gap-y-8 lg:min-h-[300px]">
-      <Image
-        src={illustRight}
-        alt=""
-        className="absolute max-w-full max-h-full bottom-0 right-0"
-      />
-      <Image
-        src={illustLeft}
-        alt=""
-        className="absolute max-w-full max-h-full top-0 left-0"
-      />
-      <h4 className=" lg:text-4xl xl:text-[40px]">
+    <section className="relative shorten w-full text-white flex flex-col justify-center items-center gap-y-8 min-h-[300px]">
+      <div className="absolute inset-0">
+        <Image
+          src={illustRight}
+          alt=""
+          className="w-full h-full object-cover"
+          layout="fill"
+        />
+      </div>
+      <div className="absolute inset-0">
+        <Image
+          src={illustLeft}
+          alt=""
+          className="w-full h-full object-cover"
+          layout="fill"
+        />
+      </div>
+      <h4 className="text-2xl lg:text-4xl xl:text-[40px] text-center px-6">
         Revolutionizing Link Optimization
       </h4>
-      <button className="lg:py-[18px] lg:px-[56px] bg-primaryColor rounded-full">
+      <button className="py-3 px-8 bg-primaryColor rounded-full">
         Get Started
       </button>
     </section>
-  );
+  )
 }
 
 export default CTA
