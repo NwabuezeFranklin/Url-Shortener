1c79b86bf5e484730258ae1d8dfbe1b53fd711be
NwabuezeFranklin <nwafrank2017@gmail.com>
2023-06-29 12:24:52 +0100

    Deploy QR  size errors  fixed

--- components/generate-qr-modal/index.tsx
+++ components/generate-qr-modal/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { FaDownload } from 'react-icons/fa'
 import { saveAs } from 'file-saver'
 import { toast } from 'react-toastify'
@@ -15,40 +15,74 @@ const GenerateQRCodeModal = ({
   QRCodeImageLink,
 }: GenerateQRModalProps) => {
   const [isDownloading, setIsDownloading] = useState<boolean>(false)
-  const [windowWidth, setWindowWidth] = useState<number>(0)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth)
-    }
-
-    handleResize()
-
-    window.addEventListener('resize', handleResize)
-
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
 
   const handleDownload = () => {
     setIsDownloading(true)
     try {
-      // Rest of the code...
+      const canvas = document.createElement('canvas')
+      canvas.width = 400
+      canvas.height = 400
+      const ctx = canvas.getContext('2d')
+      // Draw white rounded background
+      const borderRadius = 10 // Set the desired border radius
+      ctx!.fillStyle = '#FFFFFF'
+      ctx!.beginPath()
+      ctx!.moveTo(borderRadius, 0)
+      ctx!.lineTo(canvas.width - borderRadius, 0)
+      ctx!.arc(
+        canvas.width - borderRadius,
+        borderRadius,
+        borderRadius,
+        1.5 * Math.PI,
+        0
+      )
+      ctx!.lineTo(canvas.width, canvas.height - borderRadius)
+      ctx!.arc(
+        canvas.width - borderRadius,
+        canvas.height - borderRadius,
+        borderRadius,
+        0,
+        0.5 * Math.PI
+      )
+      ctx!.lineTo(borderRadius, canvas.height)
+      ctx!.arc(
+        borderRadius,
+        canvas.height - borderRadius,
+        borderRadius,
+        0.5 * Math.PI,
+        Math.PI
+      )
+      ctx!.lineTo(0, borderRadius)
+      ctx!.arc(borderRadius, borderRadius, borderRadius, Math.PI, 1.5 * Math.PI)
+      ctx!.closePath()
+      ctx!.fill()
 
       const qrCodeImage: HTMLImageElement = new Image()
       qrCodeImage.crossOrigin = 'anonymous'
       qrCodeImage.src = QRCodeImageLink
       qrCodeImage.onload = () => {
-        // Rest of the code...
+        // Calculate the center position of the canvas
+        const centerX = canvas.width / 2
 
-        const qrCodeWidth = windowWidth >= 640 ? 200 : 150
-        const qrCodeHeight = qrCodeWidth
+        // Draw the brand name
+        ctx!.font = '20px sans-serif'
+        ctx!.fillStyle = '#005AE2'
+        ctx!.textAlign = 'center'
+        const brandNameY = 50 // Set the desired Y position of the brand name
+        ctx!.fillText('Scissor.com', centerX, brandNameY)
+
+        // Draw the QR code image
+        const qrCodeWidth = 200
+        const qrCodeHeight = 200
         const qrCodeX = centerX - qrCodeWidth / 2
-        const qrCodeY = brandNameY + (windowWidth >= 640 ? 30 : 20)
+        const qrCodeY = brandNameY + 30 // Set the desired Y position of the QR code image
         ctx!.drawImage(qrCodeImage, qrCodeX, qrCodeY, qrCodeWidth, qrCodeHeight)
-
-        // Rest of the code...
+        canvas.toBlob((blob) => {
+          saveAs(blob!, 'scissor.com-qr-code.png')
+          setIsDownloading(false)
+          toast.success('QR Code download successfull!')
+          setShowQRCodeModal(false)
+        })
       }
     } catch (error) {
       console.log(error)
@@ -58,12 +92,12 @@ const GenerateQRCodeModal = ({
 
   return (
     <div
-      className={`fixed z-[200] inset-0 items-center justify-center ${
+      className={`fixed z-[200] inset-0  items-center justify-center ${
         showQRcodeModal ? 'flex' : 'hidden'
       }`}
     >
       <div className="fixed inset-0 bg-black opacity-75"></div>
-      <div className="relative z-10 bg-white rounded-lg shadow-lg p-6 min-w-[300px]">
+      <div className="relative z-10 bg-white rounded-lg shadow-lg p-6 lg:min-w-[400px]">
         <div className="flex items-center justify-between mb-4">
           <h2 className="text-xl text-primaryColor font-semibold">
             Scissor.com
@@ -93,9 +127,8 @@ const GenerateQRCodeModal = ({
             <img src={QRCodeImageLink} alt="QR Code" />
           </div>
           <button
-            className={`flex items-center gap-x-2 px-3 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ${
-              isDownloading && 'cursor-not-allowed'
-            }`}
+            className={`flex items-center gap-x-2 px-3 py-2 bg-blue-500 text-white rounded hover:bg-blue-600
+            ${isDownloading && 'cursor-not-allowed'}`}
             onClick={handleDownload}
             disabled={isDownloading}
           >
